d302ac82aab23354723b72efc77dc37608e83c13
ingridguan11-commits <ingridguan11@gmail.com>
2026-06-12 14:04:03 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,12 @@
 /** @type {import('next').NextConfig} */
-const basePath = '/portfolio'
-
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
-  basePath,
-  assetPrefix: `${basePath}/`,
   env: {
-    NEXT_PUBLIC_BASE_PATH: basePath,
+    NEXT_PUBLIC_BASE_PATH: '',
   },
   typescript: {
     ignoreBuildErrors: true,
