7f46a612b3bbbd9fa41d16cc0223308c2b45a3b1
ingridguan11-commits <ingridguan11@gmail.com>
2026-06-12 14:20:08 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
   images: {
     unoptimized: true,
-  },
-  env: {
-    NEXT_PUBLIC_BASE_PATH: '',
   },
   typescript: {
     ignoreBuildErrors: true,
